140e067cd486ed71d99493d6881723b0839e3321
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-08 17:26:34 +0530

    added feature of computer playing like computer

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -109,13 +109,13 @@ public class TicTacToeGame {
 		if (player.USER == player) {
 			displayBoardUC3();
 			abilityToMakeMoveForPlayerUC4();
-			determineNextMoveUC7(player.COMPUTER);
 			if (checkifPlayerWon()) {
 				System.out.println("Player won");
 				return;
 			}
+			determineNextMoveUC7(player.COMPUTER);
 		} else {
-			abilityToMakeMoveForComputer();
+			abilityToMakeMoveForComputerUC8();
 			if (checkifPlayerWon()) {
 				System.out.println("Player won");
 				return;
@@ -124,21 +124,114 @@ public class TicTacToeGame {
 		}
 	}
 
-	private boolean checkifPlayerWon() {
-		for(int i = 1 ;i<=7;i=i+3) {
-			if(board[i]==playerChar&&board[i+1]==playerChar&&board[i]==playerChar) {
-				return true;
-			}
+	private void abilityToMakeMoveForComputerUC8() {
+		// check in row1
+		if (board[1] != playerChar && board[2] != playerChar && board[3] != playerChar) {
+			if (board[1] == ' ')
+				board[1] = computerChar;
+			else if (board[2] == ' ')
+				board[2] = computerChar;
+			else if (board[3] == ' ')
+				board[3] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
 		}
-		for(int i = 1; i<=3 ;i++) {
-			if(board[i]==playerChar&&board[i+3]==playerChar&&board[i+6]==playerChar) {
-				return true;
-			}
+
+		// check in row2
+		if (board[4] != playerChar && board[5] != playerChar && board[6] != playerChar) {
+			if (board[4] == ' ')
+				board[4] = computerChar;
+			else if (board[5] == ' ')
+				board[5] = computerChar;
+			else if (board[6] == ' ')
+				board[6] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
 		}
-		return (board[1]==playerChar&&board[5]==playerChar&&board[9]==playerChar)||(board[3]==playerChar&&board[5]==playerChar&&board[7]==playerChar);
+
+		// check in row3
+		if (board[7] != playerChar && board[8] != playerChar && board[9] != playerChar) {
+			if (board[7] == ' ')
+				board[7] = computerChar;
+			else if (board[8] == ' ')
+				board[8] = computerChar;
+			else if (board[9] == ' ')
+				board[9] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
+		// check in column 1
+		if (board[1] != playerChar && board[4] != playerChar && board[7] != playerChar) {
+			if (board[1] == ' ')
+				board[1] = computerChar;
+			else if (board[4] == ' ')
+				board[4] = computerChar;
+			else if (board[7] == ' ')
+				board[7] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
+		// check in column 2
+		if (board[2] != playerChar && board[5] != playerChar && board[8] != playerChar) {
+			if (board[2] == ' ')
+				board[2] = computerChar;
+			else if (board[5] == ' ')
+				board[5] = computerChar;
+			else if (board[8] == ' ')
+				board[8] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
+		// check in column 3
+		if (board[3] != playerChar && board[6] != playerChar && board[9] != playerChar) {
+			if (board[3] == ' ')
+				board[3] = computerChar;
+			else if (board[6] == ' ')
+				board[6] = computerChar;
+			else if (board[9] == ' ')
+				board[9] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
+		// check in diagonal 1
+		if (board[1] != playerChar && board[5] != playerChar && board[9] != playerChar) {
+			if (board[1] == ' ')
+				board[1] = computerChar;
+			else if (board[5] == ' ')
+				board[5] = computerChar;
+			else if (board[9] == ' ')
+				board[9] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
+		// check in diagonal 2
+		if (board[3] != playerChar && board[5] != playerChar && board[7] != playerChar) {
+			if (board[3] == ' ')
+				board[3] = computerChar;
+			else if (board[5] == ' ')
+				board[5] = computerChar;
+			else if (board[7] == ' ')
+				board[7] = computerChar;
+			else
+				System.out.println("Player Lost");
+			return;
+		}
+
 	}
 
-	private void abilityToMakeMoveForComputer() {
+	private void abilityToMakeMoveForComputer1() {
 		while (true) {
 			int index = new Random().nextInt(10);
 			if (index < 1) {
@@ -153,11 +246,26 @@ public class TicTacToeGame {
 		}
 	}
 
+	private boolean checkifPlayerWon() {
+		for (int i = 1; i <= 7; i = i + 3) {
+			if (board[i] == playerChar && board[i + 1] == playerChar && board[i] == playerChar) {
+				return true;
+			}
+		}
+		for (int i = 1; i <= 3; i++) {
+			if (board[i] == playerChar && board[i + 3] == playerChar && board[i + 6] == playerChar) {
+				return true;
+			}
+		}
+		return (board[1] == playerChar && board[5] == playerChar && board[9] == playerChar)
+				|| (board[3] == playerChar && board[5] == playerChar && board[7] == playerChar);
+	}
+
 	public static void main(String args[]) {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		char[] ticTacToeBoard = ticTacToeGame.createAndReturnBoardUC1();
 		ticTacToeGame.allowPlayerToChoseFromXandOUC2();
-		ticTacToeGame.displayBoardUC3();
+		// ticTacToeGame.displayBoardUC3();
 		// int index = ticTacToeGame.abilityToMakeMoveForPlayerUC4();
 		// ticTacToeGame.displayBoardUC3();
 		Player player = ticTacToeGame.determineWhoPlaysFirstUC6();
