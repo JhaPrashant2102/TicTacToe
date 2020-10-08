6ac53848e47b4cf1d3706c81fea035d6fb159668
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-10-08 12:07:40 +0530

    added feature for user to play his move and check the index is free

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,11 +1,13 @@
 package com.capgemini.tictactoe;
+
 import java.util.*;
+
 public class TicTacToeGame {
 
 	private char[] board;
 	private char playerChar;
 	private char computerChar;
-	
+
 	public char getPlayerChar() {
 		return playerChar;
 	}
@@ -15,7 +17,7 @@ public class TicTacToeGame {
 	}
 
 	public void setComputerChar(char playerChar) {
-		if(playerChar == 'O')
+		if (playerChar == 'O')
 			this.computerChar = 'X';
 		else
 			this.computerChar = 'O';
@@ -28,19 +30,19 @@ public class TicTacToeGame {
 
 	/**
 	 * UC1
+	 * 
 	 * @return array of character
 	 */
 	private char[] createAndReturnBoardUC1() {
 		this.board = new char[10];
 		for (int index = 1; index < 10; index++) {
-			board[index] = 'X';
+			board[index] = ' ';
 		}
 		return board;
 	}
-	
+
 	/**
-	 * UC2
-	 * set player and computer character
+	 * UC2 set player and computer character
 	 */
 	private void allowPlayerToChoseFromXandOUC2() {
 		Scanner sc = new Scanner(System.in);
@@ -48,23 +50,33 @@ public class TicTacToeGame {
 		this.setPlayerChar(sc.next().charAt(0));
 		this.setComputerChar(this.playerChar);
 	}
-	
-	private void diplayBoardUC3() {
-		for(int row = 0;row<3;row++) {
-			for(int col = row*3+1;col<row*3+4;col++) {
-				System.out.print(board[col]);
+
+	private void displayBoardUC3() {
+		for (int row = 0; row < 3; row++) {
+			System.out.print("| ");
+			for (int col = row * 3 + 1; col < row * 3 + 4; col++) {
+				System.out.print(board[col] + " | ");
 			}
 			System.out.println(" ");
 		}
 	}
-	
+
+	private void abilityToMakeMoveForPlayerUC4() {
+		System.out.println("Enter the position where you want to add : " + playerChar);
+		Scanner sc = new Scanner(System.in);
+		int index = sc.nextInt();
+		if (board[index] == ' ')
+			board[index] = playerChar;
+		else
+			System.out.println("This position is already filled");
+	}
+
 	public static void main(String args[]) {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		char[] ticTacToeBoard = ticTacToeGame.createAndReturnBoardUC1();
 		ticTacToeGame.allowPlayerToChoseFromXandOUC2();
-		ticTacToeGame.diplayBoardUC3();
+		ticTacToeGame.displayBoardUC3();
+		ticTacToeGame.abilityToMakeMoveForPlayerUC4();
+		ticTacToeGame.displayBoardUC3();
 	}
-
-
-
 }
